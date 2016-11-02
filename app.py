1b8883347e769af2a22170b57786c00e0d0b5ca3
Justin Middler <jusso15@gmail.com>
2016-11-02 16:37:47 +1100

    Successfully have password hasing working

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template, request, url_for, redirect, session
-from model import client
-from flask_login import LoginManager, login_required
-import bcrypt
+from flask import Flask, render_template, request, url_for, redirect, session, g
+from model import User, user_datastore
+from flask.ext.security import Security, MongoEngineUserDatastore, \
+UserMixin, login_required
 import os
+from werkzeug import security as sec
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-db = client.user
-
 @app.route("/")
 def index():
     gettingStarted = 'Getting Started'
@@ -15,7 +13,7 @@ def index():
     return render_template('index.html',
     gettingstarted=gettingStarted, login=login)
     
-    
+
 @app.route('/register', methods=['POST', 'GET'])
 def register():
 
@@ -26,20 +24,18 @@ def register():
     if request.method == 'POST':
 
         # - Check for exsisting User
-
-        exsistingUser = db.user.find_one({"email" : request.form['email']})
+        exsistingUser = user_datastore.find_user(email=request.form['email'])
         
         if exsistingUser is not None:
 
-            return 'That email already exsists, need to reset your password?'
+            return 'That email already exsists, need to reset your password?'   
+        
+        user = user_datastore.create_user(name=request.form['firstname'], 
+        email=request.form['email'], 
+        password=request.form['password'])
+        user_datastore.commit()
 
-        user = db.user.insert_one({"firstname" : request.form['firstname']})
-        email = db.user.insert_one({"email" : request.form['email']})
-        password = db.user.insert_one({"password" : request.form['password']})
-
-        return 'Success you registered!'
-
-        session['firstname'] = request.form['firstname']      
+        return 'Success you registered!'   
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -49,23 +45,22 @@ def login():
         return render_template('login.html')
 
     if request.method == 'POST':
-        
-        user = db.user.find_one({"email" : request.form['email']})
-        password = db.user.find_one({"password" : request.form['password']})
 
-    if user and password is not None:
-        
-        return 'Welcome'
-        session['firstname'] = True
- 
-    return render_template('error.html')
+        user = user_datastore.find_user(name=request.form['firstname'])
+        email = user_datastore.find_user(email=request.form['email'])
+        password = user_datastore.find_user(password=request.form['password'])
+
+        if user and email and password is not None:
+            
+            return redirect(url_for('index'))
+            
+        return render_template('error.html')
         
 @app.route('/logout')
-@login_required    
 def logout():
 
-    return redirect(url_for('index'), logout=logout)   
+    return redirect(url_for('index'))   
 
 
 if __name__ == "__main__":
-    app.run(threaded=True,port=5000, debug=True)
+    app.run(threaded=True,debug=True, port=5000)
--- model.py
+++ model.py
@@ -1,7 +1,32 @@
-from flask import Flask
-from pymongo import MongoClient
+from flask import Flask, request, redirect, url_for
+from flask.ext.mongoengine import MongoEngine
+from flask.ext.security import Security, MongoEngineUserDatastore, \
+    UserMixin, RoleMixin, login_required
+import os
+from securemongoengine.fields import *
+
+_key = 'workingWithAES256AlgorithmKey32B'
 
 app = Flask(__name__)
-client = MongoClient()
+app.config['SECRET_KEY'] = os.urandom(16)
+# MongoDB Config
+app.config['MONGODB_DB'] = 'KeepingUpWithCass'
+app.config['MONGODB_HOST'] = 'localhost'
+app.config['MONGODB_PORT'] = 27017
 
+db = MongoEngine(app)
 
+class Role(db.Document, RoleMixin):
+    name = db.StringField(max_length=80, unique=True)
+    description = db.StringField(max_length=255)
+
+class User(db.Document, UserMixin):
+    name = db.StringField(max_length=255)
+    email = db.StringField(max_length=255, unique=True)
+    password = EncryptedStringField(key=_key,max_length=40, required=True)
+    active = db.BooleanField(default=True)
+    confirmed_at = db.DateTimeField()
+    roles = db.ListField(db.ReferenceField(Role), default=[])
+
+user_datastore = MongoEngineUserDatastore(db, User, Role)
+security = Security(app, user_datastore)
